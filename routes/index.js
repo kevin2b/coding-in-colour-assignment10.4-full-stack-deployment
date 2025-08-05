620d4b1fa578eaf74d8fc8950d321e125452d992
kevin2b <25062561+kevin2b@users.noreply.github.com>
2025-08-05 10:45:52 -0400

    Assignment 10.2, add database connection

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 // empty for assignment 10.1, Add code below for assignment 10.2
+const pool = require('../config/db');
 
-
-
-
+router.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.send(`Connected: ${result.rows[0].now}`);
+  } catch (err) {
+    res.status(500).send('Database connection failed');
+  }
+});
 
 module.exports = router;
